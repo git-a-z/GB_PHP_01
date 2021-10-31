53d6f1280cfccdd3c9113ebbe2ae31879092f952
a_z <azdvd@mail.ru>
2021-10-31 15:54:55 +0300

    GB_PHP_01_lesson_08

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product{id}", name="product")
+     */
+    public function index(): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+}
